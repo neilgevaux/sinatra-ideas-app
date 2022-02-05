aa1e057ebd744902f72ea3a30880f925e26fb3fa
Neil Gevaux <neilgevaux@gmail.com>
2022-02-05 17:32:27 +0000

    Added new erb pages

--- app.rb
+++ app.rb
@@ -15,6 +15,17 @@ get '/' do
     erb :index
   end
 
+get '/ideas' do
+    erb :ideas
+end
+
+post '/create' do
+    @title = 'Your idea was added'
+     # Set value from form input (name) as instance variable
+     @new_idea = params[:name]
+    erb :create
+end
+
 # Read
 
 
